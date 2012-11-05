e88f6c860688c1e08c5e702db274cab178aa4425
Alex Athanasopoulos <alex@melato.org>
2012-11-05 11:53:07 +0200

    added copyright

--- src/main/java/org/melato/xml/XMLTag.java
+++ src/main/java/org/melato/xml/XMLTag.java
@@ -1,5 +1,20 @@
-/* $Header: c:\\cvsroot/dev/java/aa/xml/XMLTag.java,v 1.1 2007/12/01 22:56:36 athana Exp $
- * Copyright (c) Alex Athanasopoulos 2007
+/*-------------------------------------------------------------------------
+ * Copyright (c) 2012, Alex Athanasopoulos.  All Rights Reserved.
+ * alex@melato.org
+ *-------------------------------------------------------------------------
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *-------------------------------------------------------------------------
  */
 package org.melato.xml;
 
--- src/main/java/org/melato/xml/package-info.java
+++ src/main/java/org/melato/xml/package-info.java
@@ -1,7 +1,21 @@
-/* $Header: c:\\cvsroot/dev/java/aa/xml/package-info.java,v 1.1 2007/12/01 22:56:36 athana Exp $
- * Copyright (c) Alex Athanasopoulos 2007
+/*-------------------------------------------------------------------------
+ * Copyright (c) 2012, Alex Athanasopoulos.  All Rights Reserved.
+ * alex@melato.org
+ *-------------------------------------------------------------------------
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *-------------------------------------------------------------------------
  */
-
 /** A package that defines a modular XML parsing framework on top of SAX.
  *  The main interface in this package is XMLElementHandler.
  *  A typical parser for an XML file format would define an XMLElementHandler
